c2b666a9024b8aa6e6369b3fa351fb526f8e4745
timfel20 <timmyfelicia@gmail.com>
2023-10-01 20:15:08 +0200

    Add comantarioLinea java class to repository

---
+++ comentarioLinea/src/comentarioLinea/ComentarioLinea.java
@@ -0,0 +1,9 @@
+package comentarioLinea;
+
+public class ComentarioLinea{
+    public static void main(String args[]){ 
+        //System.out.println("Hola"); 
+        System.out.println("Adíos");
+        System.out.println("FIN DE PROGRAMA");
+    }
+}
